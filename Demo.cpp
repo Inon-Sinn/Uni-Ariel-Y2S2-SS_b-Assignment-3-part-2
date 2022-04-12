e6e5019cbc523d84a037d3aed62ecb1222cf48e8
Inon Sinn <inon.sinn@gmail.com>
2022-04-12 18:35:47 +0300

    update

---
+++ Demo.cpp
@@ -0,0 +1,26 @@
+/**
+ * Demo file for the exercise on numbers with units
+ *
+ * @author Erel Segal-Halevi
+ * @since 2019-02
+ * 
+ * Edited by Tal Zichlinsky
+ * @since 2022-02
+ */
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+using namespace std;
+#include <vector>
+
+#include "Matrix.hpp"
+using namespace zich;
+
+int main() {
+
+  Matrix a{{},3,3};
+  cout << "End of demo!" << endl;
+  return 0;
+}
